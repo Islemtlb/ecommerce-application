1ee987fc47da5e11e8017fbca39c325ad5f27412
Islemtlb <islam.talbi96@gmail.com>
2024-03-20 16:32:44 +0100

    added product riverpod

---
+++ lib/controller/product_riverpod.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:ecom/model/product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:riverpod/riverpod.dart';
+
+ final productsProvider = FutureProvider<String>((ref) async {
+  return await rootBundle.loadString('assets/products.json');
+});
+
+ final parsedProductsProvider = Provider<List<ProductModel>>((ref) {
+  final jsonString = ref.watch(productsProvider).asData?.value;
+  if (jsonString != null) {
+    final jsonData = json.decode(jsonString);
+    return List<ProductModel>.from(jsonData['products'].map((x) => ProductModel(
+      category: x['category'],
+      name: x['name'],
+      price: x['price'].toDouble(),
+      description: x['description'],
+      imagePath: x['image_path'],
+    )));
+  }
+  return [];
+});
+class ProductRiverpod extends ChangeNotifier {
+  
+  
+}
--- lib/controller/riverpod_management.dart
+++ lib/controller/riverpod_management.dart
@@ -1,11 +1,5 @@
 
-
-
 import 'package:ecom/controller/product_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-
-   
-
 final productRiverpod = ChangeNotifierProvider((_) => ProductRiverpod());
 
