3e642fa414e74103483e6eb82923bdd21d3c7572
Islemtlb <islam.talbi96@gmail.com>
2024-03-20 16:29:26 +0100

    added product model and mapping

---
+++ lib/controller/riverpod_management.dart
@@ -0,0 +1,11 @@
+
+
+
+import 'package:ecom/controller/product_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+
+   
+
+final productRiverpod = ChangeNotifierProvider((_) => ProductRiverpod());
+
---
+++ lib/model/product.dart
@@ -0,0 +1,18 @@
+
+
+class ProductModel {
+  final String category;
+  final String name;
+  final double price;
+  final String description;
+  final String imagePath;
+
+  ProductModel({
+    required this.category,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imagePath,
+  });
+}
+
